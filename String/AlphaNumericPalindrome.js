eb83bdba609a30ddd81e463e1f9fb91407d8b390
sainesh31 <saineshkchandani31@gmail.com>
2025-03-19 10:34:15 +0530

    Initial commit

--- String/AlphaNumericPalindrome.js
+++ String/AlphaNumericPalindrome.js
@@ -22,6 +22,8 @@ const alphaNumericPalindrome = (str) => {
   // removing all the special characters and turning everything to lowercase
   const newStr = str.replace(/[^a-z0-9]+/gi, '').toLowerCase()
   const midIndex = newStr.length >> 1 // x >> y = floor(x / 2^y)
+  console.log(newStr)
+  console.log(midIndex)
 
   for (let i = 0; i < midIndex; i++) {
     if (newStr.at(i) !== newStr.at(~i)) {
@@ -31,6 +33,27 @@ const alphaNumericPalindrome = (str) => {
   }
 
   return true
+
 }
+
+
+alphaNumericPalindrome("hello") // false
+alphaNumericPalindrome("Race a Car") // false
+alphaNumericPalindrome("123ab321") // false
+alphaNumericPalindrome("palindrome") // false
+
+
+console.log(alphaNumericPalindrome("racecar")); // true
+console.log(alphaNumericPalindrome("RaceCar")); // true
+console.log(alphaNumericPalindrome("race CAR")); // true
+console.log(alphaNumericPalindrome("2A3*3a2")); // true
+console.log(alphaNumericPalindrome("2A3 3a2")); // true
+console.log(alphaNumericPalindrome("2_A3*3#A2")); // true
+
+console.log(alphaNumericPalindrome("hello")); // false
+console.log(alphaNumericPalindrome("Race a Car")); // false
+console.log(alphaNumericPalindrome("123ab321")); // false
+console.log(alphaNumericPalindrome("palindrome")); // false
+
 
 export default alphaNumericPalindrome
