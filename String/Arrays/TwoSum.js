407ef93b89c8e89f88c42160fa3c41c7f2966c47
sainesh31 <saineshkchandani31@gmail.com>
2025-03-25 04:44:20 +0530

    twosum

--- String/Arrays/TwoSum.js
+++ String/Arrays/TwoSum.js
@@ -1,20 +1,39 @@
-function twoSum(nums, target) {
-  debugger
-  let map = new Map()
+// brute force
 
+function twoSumBruteForce(nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    let complement = target - nums[i]
-
-    if (map.has(complement)) {
-      return [map.get(complement), i]
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j] // Return indices if a pair is found
+      }
     }
-
-    map.set(nums[i], i)
-    console.log(map)
   }
-
-  return []
+  return [] // Return empty array if no pair is found
 }
 
 // Example usage:
-console.log(twoSum([2, 7, 11, 15], 9)) // Output: [0, 1]
+console.log(twoSumBruteForce([2, 7, 11, 15], 18)) // Output: [0, 1]
+
+// optimal solution
+
+// function twoSum(nums, target) {
+//   debugger
+
+//   let map = new Map()
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let complement = target - nums[i]
+
+//     if (map.has(complement)) {
+//       return [map.get(complement), i]
+//     }
+
+//     map.set(nums[i], i)
+//     console.log(map)
+//   }
+
+//   return []
+// }
+
+// // Example usage:
+// console.log(twoSum([2, 7, 11, 15], 18)) // Output: [0, 1]
