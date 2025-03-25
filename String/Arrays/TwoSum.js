5703829c64d9ad7a8d72c44eaf8dbf76d4099ffc
sainesh31 <saineshkchandani31@gmail.com>
2025-03-25 05:36:03 +0530

    twosum using 2pointer

--- String/Arrays/TwoSum.js
+++ String/Arrays/TwoSum.js
@@ -1,18 +1,18 @@
-// brute force
+// // brute force
 
-function twoSumBruteForce(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j] // Return indices if a pair is found
-      }
-    }
-  }
-  return [] // Return empty array if no pair is found
-}
+// function twoSumBruteForce(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j] // Return indices if a pair is found
+//       }
+//     }
+//   }
+//   return [] // Return empty array if no pair is found
+// }
 
-// Example usage:
-console.log(twoSumBruteForce([2, 7, 11, 15], 18)) // Output: [0, 1]
+// // Example usage:
+// console.log(twoSumBruteForce([2, 7, 11, 15], 18)) // Output: [0, 1]
 
 // optimal solution
 
@@ -37,3 +37,29 @@ console.log(twoSumBruteForce([2, 7, 11, 15], 18)) // Output: [0, 1]
 
 // // Example usage:
 // console.log(twoSum([2, 7, 11, 15], 18)) // Output: [0, 1]
+
+// optimal soltion if sorted using two pointer greedy approach
+function twoSumTwoPointer(nums, target) {
+  debugger
+  let l = 0,
+    r = nums.length - 1
+  let sortedNums = nums
+    .map((nums, index) => ({ nums, index }))
+    .sort((a, b) => a.nums - b.nums)
+  console.log(sortedNums)
+
+  while (l < r) {
+    let sum = sortedNums[l].nums + sortedNums[r].nums
+    if (sum === target) {
+      return [sortedNums[l].index, sortedNums[r].index]
+    } else if (sum > target) {
+      r--
+    } else {
+      l++
+    }
+  }
+  return []
+}
+
+// Example usage:
+console.log(twoSumTwoPointer([2, 11, 7, 15], 9)) // Output: [0, 1]
